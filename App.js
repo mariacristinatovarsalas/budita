071a0f9cc8144a26d91f2f3d04e3f8486b3a4b15
Maria Cristina <58868315+mariacristinatovarsalas@users.noreply.github.com>
2020-10-06 01:14:19 -0300

    Expo eject process

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>INIAPP</Text>
+      <Text>INIAPP!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
